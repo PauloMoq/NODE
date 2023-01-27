3e5b73a9d2408705c7b909ef0d80c9f82e07f1db
HURDEBOURCQ Paul <phurdepro@gmail.com>
2023-01-27 15:48:04 +0100

    findone

---
+++ TP2/src/services/db/crud.js
@@ -0,0 +1,19 @@
+const { getCollection } = require("./connection");
+
+async function findOne(collectionName, query, options = {}) {
+  try {
+    const collection = getCollection(collectionName);
+    const result = await collection.findOne(query, options);
+    return result;
+  } catch (e) {
+    console.log(
+      `Erreur lors de l execution de la fonction findOne avec les parametres suivants: ${query}`
+    );
+    console.log(e);
+    throw e;
+  }
+}
+
+module.exports = {
+  findOne,
+};
