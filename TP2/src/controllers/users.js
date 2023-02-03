2b5329de5065fc19965a125b479d793f7973442c
Paulo <105054182+PauloMoq@users.noreply.github.com>
2023-02-03 09:24:19 +0100

    Delete TP2 directory

--- TP2/src/controllers/users.js
+++
@@ -1,8 +0,0 @@
-function createUser(req, res, next) {
-  console.log("cration ....");
-  res.send("dfsonkjlsdfn");
-}
-
-module.exports = {
-  createUser,
-};
--- TP2/src/routes/users.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createUser } = require("../controllers/users");
-
-router.get("/create", createUser);
-
-module.exports = router;
